e2b349d9f9b0d07426190766e24ffb696d9aed6d
Dan Moneypenny <catullus1973@gmail.com>
2017-12-28 21:17:26 -0500

    Changed text in html and json files

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@
 
 var friends = [
   {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Willie McCovey",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/01.jpg",
     scores: [
       "5",
       "1",
@@ -22,8 +22,8 @@ var friends = [
     ]
   },
   {
-    name: "Jacob Deming",
-    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    name: "Johnnie LeMaster",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/02.jpg",
     scores: [
       "4",
       "2",
@@ -38,8 +38,8 @@ var friends = [
     ]
   },
   {
-    name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    name: "Will Clark",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/03.jpg",
     scores: [
       "5",
       "2",
@@ -54,8 +54,8 @@ var friends = [
     ]
   },
   {
-    name: "Louis T. Delia",
-    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    name: "Barry Bonds",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/04.jpg",
     scores: [
       "3",
       "3",
@@ -70,8 +70,8 @@ var friends = [
     ]
   },
   {
-    name: "Lou Ritter",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    name: "Jason Schmidt",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/05.jpg",
     scores: [
       "4",
       "3",
@@ -86,8 +86,8 @@ var friends = [
     ]
   },
   {
-    name: "Jordan Biason",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    name: "Matt Cain",
+    photo: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/230557/06.jpg",
     scores: [
       "4",
       "4",
